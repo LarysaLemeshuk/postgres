2e91e14b4ca530380c36647fca01399872268178
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-11-14 18:01:43 +0100

    creating table

--- Local PostgreSQL.session.sql
+++ Local PostgreSQL.session.sql
@@ -1,1 +1,9 @@
-CREATE TABLE test1();
+CREATE TABLE users_with_emails();
+
+DROP TABLE users,users_with_emails,test;
+
+CREATE TABLE users (
+    first_name varchar(64),
+    last_name varchar(64),
+    biography text
+)
