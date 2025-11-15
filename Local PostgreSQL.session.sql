b1e47d3e1bdc187ff3388bab7c9bd7d8a4e71824
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-11-15 18:19:36 +0100

    Inserting data into a table

--- Local PostgreSQL.session.sql
+++ Local PostgreSQL.session.sql
@@ -1,9 +1,17 @@
-CREATE TABLE users_with_emails();
+CREATE TABLE users(
+    first_name VARCHAR(64),
+    last_name VARCHAR(64),
+    biography text,
+    gender VARCHAR(30),
+    is_subscribe boolean,
+    birthday date,
+    foot_size smallint,
+    height numeric(5, 2)
+);
 
-DROP TABLE users,users_with_emails,test;
+INSERT INTO users VALUES('John', 'Doe', 'Тут може бути велика розповідь про ДЖона', 'male', true, '2004-09-14', 43, 1.85);
 
-CREATE TABLE users (
-    first_name varchar(64),
-    last_name varchar(64),
-    biography text
-)
+INSERT INTO users VALUES('Susan', 'Doe', 'Тут може бути велика розповідь про Сьюзан', 'female', true, '2004-09-15', 38, 1.65),
+('Peter', 'Doe', 'Тут може бути велика розповідь про Пітера', 'male', true, '2004-12-14', 45, 1.92);
+
+INSERT INTO users VALUES('Blaeke', 'Doe', 'Тут може бути велика розповідь про Блейка', 'male', false, '2000-10-14', NULL, NULL);
