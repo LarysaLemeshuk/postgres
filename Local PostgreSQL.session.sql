d189e7b8fc12b877feb62f8cddc434f93c73814d
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-12-26 15:41:05 +0100

    Chats to users task resolve

--- Local PostgreSQL.session.sql
+++ Local PostgreSQL.session.sql
@@ -1,28 +1,36 @@
-
-
-CREATE TABLE orders(
+CREATE TABLE chats(
     id serial PRIMARY KEY,
-    created_at timestamp NOT NULL DEFAULT current_timestamp,
-    custumer_id int REFERENCES users(id)
+    name varchar(256) NOT NULL CHECK(name != ''),
+    owner_id int REFERENCES users(id),
+    created_at timestamp DEFAULT current_timestamp
 );
 
-DROP TABLE orders;
-ALTER TABLE users
-ADD COLUMN id serial PRIMARY KEY;
+INSERT INTO chats(name, owner_id)
+VALUES -- створенння чату
+    ('superchats', 2);
 
-CREATE TABLE oreders_to_produts(
-    product_id int REFERENCES products(id),
-    orders_id int REFERENCES orders(id),
-    quntity int,
-    PRIMARY KEY(orders_id, product_id)
+CREATE TABLE chats_to_users(
+    chat_id int REFERENCES chats(id),
+    user_id int REFERENCES users(id),
+    join_at timestamp DEFAULT current_timestamp,
+    PRIMARY KEY (chat_id, user_id)
 );
 
+INSERT INTO chats_to_users(chat_id, user_id) VALUES -- додавання юзерів до чату
+(1, 1);
 
--- Оформлення замовлення для юзера
--- 1 створили замовлення
-INSERT INTO orders(custumer_id) VALUES(2);
--- 2 наповнити замовлення
-INSERT INTO oreders_to_produts (product_id, orders_id, quntity)VALUES
-(2, 1, 1),
-(3, 1, 2),
-(4, 1, 1);
+CREATE TABLE messages(
+    id serial PRIMARY KEY,
+    body text NOT NULL CHECK(body != ''),
+    created_at timestamp DEFAULT current_timestamp,
+    is_read boolean NOT NULL DEFAULT false,
+    -- author_id int REFERENCES chats_to_users(user_id),
+    -- chat_id int REFERENCES chats_to_users(chat_id),
+    author_id int,
+    chat_id int,
+    FOREIGN KEY (author_id, chat_id) REFERENCES chats_to_users(user_id, chat_id)
+);
+
+INSERT INTO messages(body, author_id, chat_id) VALUES
+('Go for coffe?', 1, 1), -- додавання повідомлень ддо чату
+('Go!', 2, 1);
