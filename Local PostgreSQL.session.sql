5787b6b01873142b5b031cf5cdb026170017be65
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-12-29 18:00:45 +0100

    GENERATION COLUMN WELCOME

--- Local PostgreSQL.session.sql
+++ Local PostgreSQL.session.sql
@@ -1,15 +1,23 @@
------ 1 : 1
-
-CREATE TABLE coaches(
-id serial PRIMARY KEY,
-name varchar(256) CHECK(name != '') NOT NULL
-);
-
-CREATE TABLE teams(
+/*
+ в таблиці products створити генерований стовпець is_luxury
+ 
+ якщо prise > 800 -> true
+ else -> false
+ 
+ */
+CREATE TABLE products_version_2(
     id serial PRIMARY KEY,
-    name varchar(256) CHECK(name != '') NOT NULL,
-    coach_id int REFERENCES coaches(id)
+    brand varchar(256) NOT NULL CHECK(brand != ''),
+    model varchar(256) NOT NULL CHECK(model != ''),
+    prise numeric(8, 2) NOT NULL,
+    is_luxury boolean GENERATED ALWAYS AS(prise > 800) STORED
 );
+INSERT INTO products_version_2 (brand, model, prise)
+VALUES 
+('Iphone', '15 Pro', 1300),
+('Samsung', 'S10', 400),
+('Xiaomi', 'Model 5', 200);
 
-ALTER TABLE coaches
-ADD COLUMN team_id int REFERENCES teams(id);
+
+ALTER TABLE products
+ADD COLUMN is_luxury boolean GENERATED ALWAYS AS (price > 800) STORED;
